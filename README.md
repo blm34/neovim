70e6c57119fa16bba124919bd61ed12647a811ba
blm34 <48328783+blm34@users.noreply.github.com>
2025-09-05 23:46:03 +0100

    Write remap.lua with custom vim remaps

--- README.md
+++ README.md
@@ -13,3 +13,51 @@ To use this Neovim configuration, this repository should be cloned to:
 * [git](https://git-scm.com/downloads)
 * [Ripgrep](https://github.com/BurntSushi/ripgrep?tab=readme-ov-file#installation)
 
+## Keymaps ##
+
+`<leader>` is mapped to `<space>`
+
+`<localleader>` is mapped to `\`
+
+### Editing ###
+
+| Mode | Keymap | Action | Mnemonic |
+|------|--------|--------|----------|
+| v | `J` | Move selection down | |
+| v | `K` | Move selection up | |
+| n & x | `<leader>p` | Paste from system clipboard | Paste |
+| n & v | `<leader>y` | Copy to system clipboard | Yank |
+| n & v | `<leader>d` | Delete - don't write to the unnamed register | Delete |
+| n | `<leader>s` | Global (in buffer) replace word under cursor | Substitute |
+| n | `go` | Add blank line below cursor |
+| n | `gO` | Add blank line above cursor |
+
+### Visual ###
+
+| Mode | Keymap | Action |
+|------|--------|--------|
+| n | `<ESC>` | Turn off highlight search |
+
+### Quick Fix List ###
+
+| Mode | Keymap | Action |
+|------|--------|--------|
+| n | `<C-j>` | Go to next item in the qfl |
+| n | `<C-k>` | Go to previous item in the qfl |
+| n | `<leader>j` | Got to the next item in the location list |
+| n | `<leader>k` | Got to the previous item in the location list |
+
+### Window Management ###
+
+| Mode | Keymap | Action |
+|------|--------|--------|
+| n & t | `<M-h>` | Move to window to the left |
+| n & t | `<M-l>` | Move to window to the right |
+| n & t | `<M-j>` | Move down one window |
+| n & t | `<M-k>` | Move up one window |
+| n | `<CM-h>` | Decrease window width |
+| n | `<CM-l>` | Increase window width |
+| n | `<CM-j>` | Decrease window height |
+| n | `<CM-k>` | Increase window height |
+| t | `<ESC>` | Exit terminal mode (to normal mode) |
+
---
+++ lua/config/remap.lua
@@ -0,0 +1,56 @@
+-- Set leader and local leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Move selected text with J and K in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- Stay in visual mode when indenting blocks
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
+
+-- Recenter screen on jumps
+vim.keymap.set("n", "n", function()
+    vim.cmd("normal! n")
+    require("mini.animate").execute_after("scroll", vim.cmd("normal! zzzv"))
+end)
+vim.keymap.set("n", "N", function()
+    vim.cmd("normal! N")
+    require("mini.animate").execute_after("scroll", vim.cmd("normal! zzzv"))
+end)
+
+-- Custom copy paste remaps
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("x", "<leader>p", "\"+p")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Quick fix list
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
+
+-- Global replace word under cursor
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+
+-- Move between windows with Alt+direction
+vim.keymap.set("n", "<M-h>", "<C-w>h")
+vim.keymap.set("n", "<M-l>", "<C-w>l")
+vim.keymap.set("n", "<M-j>", "<C-w>j")
+vim.keymap.set("n", "<M-k>", "<C-w>k")
+
+-- Resize windows with Ctrl+Alt+direction
+vim.keymap.set("n", "<CM-h>", function() vim.cmd("vertical resize -1") end)
+vim.keymap.set("n", "<CM-l>", function() vim.cmd("vertical resize +1") end)
+vim.keymap.set("n", "<CM-j>", function() vim.cmd("resize -1") end)
+vim.keymap.set("n", "<CM-k>", function() vim.cmd("resize +1") end)
+
+-- add blank lines in normal mode
+vim.keymap.set("n", "go", "o<ESC>k")
+vim.keymap.set("n", "gO", "O<ESC>j")
