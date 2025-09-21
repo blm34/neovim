bef2a5b361bd9dd1d53b7456e9dcbf9bbd3283d4
blm34 <48328783+blm34@users.noreply.github.com>
2025-09-21 23:24:15 +0100

    Install mini-animate plugin

---
+++ lua/plugins/ui/mini-animate.lua
@@ -0,0 +1,39 @@
+return {
+    "nvim-mini/mini.animate",
+    version = "*",
+    config = function()
+        local animate = require("mini.animate")
+
+        animate.setup({
+            cursor = {
+                enable = false,
+            },
+
+            scroll = {
+                enable = true,
+                timing = animate.gen_timing.exponential({
+                    easing = "out",
+                    duration = 300,
+                    unit = "total",
+                    easing_power = 2.0,
+                }),
+                subscroll = animate.gen_subscroll.equal({ max_output_steps = 60 }),
+            },
+
+            resize = {
+                enable = true,
+                timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+            },
+
+            open = {
+                enable = true,
+                timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+            },
+
+            close = {
+                enable = true,
+                timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+            },
+        })
+    end
+}
