260de97c5764adab567c852ccd2534849e8e5fe5
blm34 <48328783+blm34@users.noreply.github.com>
2025-09-21 16:56:21 +0100

    Install treesitter plugin

---
+++ lua/plugins/utils/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            highlight = { enable = true },
+            indent = { enable = true },
+            autotage = { enable = true },
+            ensure_installed = {
+                "lua",
+                "python",
+                "bash",
+                "csv",
+                "git_config",
+                "git_rebase",
+                "gitcommit",
+                "gitignore",
+                "html",
+                "json",
+                "markdown",
+                "markdown_inline",
+                "regex",
+                "sql",
+                "toml",
+                "vimdoc",
+                "yaml",
+            },
+            auto_install = false,
+        })
+    end
+}
