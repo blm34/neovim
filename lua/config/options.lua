9b7689c09f33589c20f61fdab8f140a675bab6bc
blm34 <48328783+blm34@users.noreply.github.com>
2025-11-30 13:05:56 +0000

    Update buffer if changed on disk but not modified in neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -24,3 +24,10 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 100
 
 vim.opt.colorcolumn = "80"
+
+-- Update buffers if changed and not modified
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd(
+    { "FocusGained", "BufEnter" },
+    { command = "checktime" }
+)
